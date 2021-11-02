c90ae13e16c7cf27a245145abe5d116f41c1b31d
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2021-11-02 15:34:45 -0300

    input file imagens

--- Praticando/createElement/script.js
+++ Praticando/createElement/script.js
@@ -19,3 +19,16 @@ function fechar(e){
     e.parentNode.outerHTML = ''
     //se fosse o div com onclick, nn teria parentNode!!!
 }
+
+
+//teste, não entendi algumas coisas aqui... mas deu certo
+function imagem(){
+    if(this.files && this.files[0]){
+        var arquivo = new FileReader()
+        arquivo.onload = function(e){
+            document.getElementById("img").src = e.target.result
+        }
+        arquivo.readAsDataURL(this.files[0])
+    }
+}
+document.getElementById("imagei").addEventListener("change", imagem, false)
