db07d52444448442c30bcf43e883dd42c18d9bdf
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2021-11-05 17:45:48 -0300

    estudando um pouco mais

---
+++ mais-de-javascript/funcoes/script.js
@@ -0,0 +1,28 @@
+/*
+funções declarativas
+function ola(x){
+    console.log(x)
+}
+
+
+var msg = 'olá mundo'
+ola(msg)
+*/
+
+
+//expressões de função
+/*pode ser:
+var x = function nomeaq(){}
+var x = function(){}
+
+var x = function(){
+    console.log("Olá mundo")
+}
+x()
+*/
+
+//Arrow function
+var y = () => {
+    console.log("arrow function aq")
+}
+y()
---
+++ mais-de-javascript/vetores-e-outros/script.js
@@ -0,0 +1,31 @@
+/*
+var ve = ['string aqui', 1, true, ['vetor1'], ['ve2']]
+
+console.log(ve)
+for(let i = 0; i < ve.length; i++){
+    console.log(ve[i])
+}
+//ve.splice(0, 2)
+//console.log(ve)
+
+var novove = ve.slice(2, 4)
+console.log(novove)
+*/
+/*
+var objeto = {
+    cara: "caractere",
+    num: 2,
+    verdade: true,
+    vetor: ['ve'],
+    objetodois: {
+        nsei: 3,
+        nome: 'segundo',
+        v: true
+    }
+}
+console.log(objeto)
+
+var {cara, num, objetodois} = objeto
+
+console.log(cara)*/
+
