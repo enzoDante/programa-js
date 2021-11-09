941d9d27a03269910d8a72c6ef96c3c9b03ad3ed
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2021-11-09 20:30:45 -0300

    melhorando um pouco o cod

--- Praticando/teste-pequenosDetalhes/botao-menu-responsivo/script.js
+++ Praticando/teste-pequenosDetalhes/botao-menu-responsivo/script.js
@@ -1,6 +1,24 @@
+var i = 1
+var menu = document.getElementsByClassName("a")
+
 function abrir(x){
     
     x.classList.toggle("change")
+    for(let a = 0; a < menu.length; a++){
+
+        menu[a].classList.toggle("teste")
+    }
 
 
+    /*if(i === 1){
+        for(let controle = 0; controle < menu.length; controle++){
+            menu[controle].style.display = 'block'
+        }
+        i++
+    }else{
+        for(let controle = 0; controle < menu.length; controle++){
+            menu[controle].style.display = 'none'
+        }
+        i--
+    }*/
 }
