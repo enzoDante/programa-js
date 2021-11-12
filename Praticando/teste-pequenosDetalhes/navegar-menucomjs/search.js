8656e1146848da41e18021efb9434551381a7b1d
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2021-11-12 18:10:00 -0300

    teste

---
+++ Praticando/teste-pequenosDetalhes/navegar-menucomjs/search.js
@@ -0,0 +1,18 @@
+function pesquisar(){
+    let a, input, valor, lista, ellista
+    input = document.getElementById("pesquisa")
+    valor = input.value.toUpperCase()
+
+    lista = document.getElementById("menupesquisa")
+    ellista = lista.getElementsByTagName("li")
+    
+    for(let i = 0; i < ellista.length; i++){
+        a = ellista[i].getElementsByTagName("a")[0]
+        if(a.innerHTML.toUpperCase().indexOf(valor) > -1){
+            ellista[i].style.display = ""
+        }else{
+        ellista[i].style.display = "none"
+        }
+    }
+
+}
