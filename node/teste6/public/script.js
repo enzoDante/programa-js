ab69eb6c08bc2ff560568290048ff5308ba3384b
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2022-10-22 19:42:21 -0300

    objetivo concluído por metade

--- node/teste6/public/script.js
+++ node/teste6/public/script.js
@@ -1,4 +1,19 @@
+function fazGet(url){
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+}
 
+function carregar(){
+    let data = fazGet("http://localhost:8081/outro")
+    let valores = JSON.parse(data)
+    console.log(valores)
+    valores.forEach(element => {
+        console.log(element)
+    });
+
+}
 
 function testar(){
     console.log('um teste 1 ')
